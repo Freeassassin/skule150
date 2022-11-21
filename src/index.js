fb3673ac35e7383a1923869f2f24af20d5b79b60
Farbod M <farbodm.dev@gmail.com>
2022-11-21 17:03:41 -0500

    changing entry file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+async function loadApp() {
+  const { React } = await import("react");
+  const { ReactDOM } = await import("ReactDOM");
+  await import("./index.css");
+  const { App } = await import("./App.js"); // this is your normal entry file - (index.js, main.js, app.mjs etc.)
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+loadApp();
