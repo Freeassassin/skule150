3a586e8384c612d293ddd45d5cd7f24315593ec2
Freeassassin <farbodemzadeh@gmail.com>
2023-01-01 23:29:23 -0500

    fixed hosting issues

--- src/App.js
+++ src/App.js
@@ -1,51 +1,55 @@
-import { BrowserRouter, useLocation, Route, Routes } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import { Navbar } from './components/Navbar/Navbar';
-import { ThemeProvider } from './utils/ThemeProvider';
-import ScrollToTop from './utils/ScrollToTop';
-import { pages } from './utils/pages';
+import { BrowserRouter, useLocation, Route, Routes } from "react-router-dom";
+import { browserHistory } from "react-router";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { Navbar } from "./components/Navbar/Navbar";
+import { ThemeProvider } from "./utils/ThemeProvider";
+import ScrollToTop from "./utils/ScrollToTop";
+import { pages } from "./utils/pages";
 
-import './App.scss';
-
+import "./App.scss";
 
 function App() {
   return (
     <ThemeProvider>
-        <BrowserRouter>
-          <TransitionRoutes />
-        </BrowserRouter>
+      <BrowserRouter>
+        <TransitionRoutes />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
-
 
 const TransitionRoutes = () => {
   const location = useLocation();
 
   return (
     <TransitionGroup>
-      <Navbar/>
+      <Navbar />
       <ScrollToTop />
       <CSSTransition key={location.key} classNames="page" timeout={300}>
         <Routes location={location}>
-        <Route path="*" element={pages['404'].component} />
-          {[
-            ...pages.main
-          ].map((page) => {
+          <Route path="*" element={pages["404"].component} />
+          {[...pages.main].map((page) => {
             return (
               <Route
                 path={page.path}
                 key={page.path}
                 element={
-                  <div style={{ position: 'absolute', right: 0, left: 0, bottom: 0, top: 0 }}>
-                    <div style={{ minHeight: '100vh' }}>{page.component}</div>
+                  <div
+                    style={{
+                      position: "absolute",
+                      right: 0,
+                      left: 0,
+                      bottom: 0,
+                      top: 0,
+                    }}
+                  >
+                    <div style={{ minHeight: "100vh" }}>{page.component}</div>
                     {/* {page.includeFooter ? <Footer /> : <></>} */}
                   </div>
                 }
               />
             );
           })}
-          
         </Routes>
       </CSSTransition>
     </TransitionGroup>
